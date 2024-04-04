ca5fa6e9c7cf62d60fb67acce03db4aa8d8a45ec
Subdij <ziyadou2011@hotmail.fr>
2024-04-04 11:45:19 +0200

    Mise à jour de la visibilité du formulaire dans js.js

--- public/js/js.js
+++ public/js/js.js
@@ -1,27 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var isFormVisible = false; // Variable pour suivre l'état du formulaire
+    var isFormVisible = false;
     var form = document.getElementById('articleForm');
     var button = document.getElementById('showFormButton');
 
     button.addEventListener('click', function(event) {
-        event.stopPropagation(); // Empêche l'événement de remonter à l'élément body
+        event.stopPropagation();
         if (isFormVisible) {
-            // Si le formulaire est visible, le cacher
             form.style.right = '-100%';
             isFormVisible = false;
         } else {
-            // Si le formulaire est caché, le montrer
             form.style.right = '0';
             isFormVisible = true;
         }
     });
 
-    // Empêche l'événement click sur le formulaire de remonter à l'élément body
     form.addEventListener('click', function(event) {
         event.stopPropagation();
     });
 
-    // Ferme le formulaire lorsque vous cliquez en dehors de la div du formulaire
     document.body.addEventListener('click', function() {
         if (isFormVisible) {
             form.style.right = '-100%';
