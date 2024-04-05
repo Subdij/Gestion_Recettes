9181e375d707a881fad5a5ace029393628e2a52b
Subdij <ziyadou2011@hotmail.fr>
2024-04-05 15:01:36 +0200

    Mise à jour du code JavaScript pour ajouter un écran de chargement

--- public/js/js.js
+++ public/js/js.js
@@ -25,3 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loading-screen');
+    setTimeout(function() {
+        loader.style.opacity = '0';
+        setTimeout(function() {
+            loader.style.display = 'none';
+        }, 1000);
+    }, 1000);
+});
